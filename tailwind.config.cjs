450baa3e0705624006329777826d5ac8e20cca60
Pablorojas07 <pablorojasbrandan@gmail.com>
2026-02-20 17:51:32 +0100

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        pistacho: 'var(--color-pistacho)',
+        rosapastel: 'var(--color-rosapastel)'
+      }
+    }
+  },
+  plugins: []
+}
